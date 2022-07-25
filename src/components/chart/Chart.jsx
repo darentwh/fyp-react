f374b057d8a17a77da0082ae6f5edee6b6022a9a
darentwh <darentan1998@gmail.com>
2022-07-25 22:51:03 +0800

    menu side bar & chart value^exp

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -14,43 +14,43 @@ export default function Chart({value}) {
     var data = [
         {
           name: "Page A",
-          uv: value*200,
+          uv: value**1,
           pv: 2400,
           amt: 2400
         },
         {
           name: "Page B",
-          uv: value*200/12,
+          uv: value**2,
           pv: 1398,
           amt: 2210
         },
         {
           name: "Page C",
-          uv: 2000,
+          uv: value**2.1,
           pv: 9800,
           amt: 2290
         },
         {
           name: "Page D",
-          uv: 2780,
+          uv: value**2.2,
           pv: 3908,
           amt: 2000
         },
         {
           name: "Page E",
-          uv: 1890,
+          uv: value**2.3,
           pv: 4800,
           amt: 2181
         },
         {
           name: "Page F",
-          uv: 2390,
+          uv: value**2.4,
           pv: 3800,
           amt: 2500
         },
         {
           name: "Page G",
-          uv: 3490,
+          uv: value**2.5,
           pv: 4300,
           amt: 2100
         }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Sidebar.css'
+import LooksOneIcon from '@mui/icons-material/LooksOne';
+import LooksTwoIcon from '@mui/icons-material/LooksTwo';
+import Looks3Icon from '@mui/icons-material/Looks3';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import InventoryIcon from '@mui/icons-material/Inventory';
@@ -11,7 +14,24 @@ export default function Sidebar(){
             <div className="sidebarWrapper">
                 <div className="siderbarMenu">
                     <h4 className="siderbarTitle">
-                        Dashboard
+                        Menu
+                    </h4>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <LooksOneIcon className="sidebarListItemIcon"/>
+                            foo
+                        </li>
+                        <li className="sidebarListItem">
+                            <LooksTwoIcon className="sidebarListItemIcon"/>
+                            bar
+                        </li>
+                        <li className="sidebarListItem">
+                            <Looks3Icon className="sidebarListItemIcon"/>
+                            baz
+                        </li>
+                    </ul>
+                    <h4 className="siderbarTitle">
+                        Functions
                     </h4>
                     <ul className="sidebarList">
                         <Link to="/forecasting" className="link">
