9b2ad99604a76aa3210725ec27b1120909c76834
darentwh <darentan1998@gmail.com>
2022-07-27 21:53:39 +0800

    fade in transition for homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,9 @@ export default function Home(){
     return(
         <div className="home">
             <div className="midWrap">
-                test
+                <div className="writings">
+                    Hello there!
+                </div>
             </div>
         </div>
     )
