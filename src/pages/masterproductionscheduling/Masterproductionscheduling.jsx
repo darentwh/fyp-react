75880734e25b9e924e569e3a5e678872f54c0b45
darentwh <darentan1998@gmail.com>
2022-07-29 20:58:41 +0800

    receiving strings from lambda api

--- src/pages/masterproductionscheduling/Masterproductionscheduling.jsx
+++ src/pages/masterproductionscheduling/Masterproductionscheduling.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
 import './Masterproductionscheduling.css'
 
 export default function Masterproductionscheduling(){
+    const [fetchedData, setFetchedData] = useState([]);
+    useEffect(() => {
+      const getData = async () => {
+        const data = await axios.get(
+          "https://xxdzzkf7xczg65izs37npoc3c40biikv.lambda-url.us-east-1.on.aws/"
+        );
+        setFetchedData(data);
+      };
+      getData();
+    }, []);
+
     return(
         <div className="mps">
             results:
+            <br/>{fetchedData.data}
         </div>
     )
 }
