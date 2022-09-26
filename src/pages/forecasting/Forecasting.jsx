9dda15bccaebf41e264d4c7bb8962cfb37913ae6
darentwh <darentan1998@gmail.com>
2022-09-26 18:34:41 +0800

    button selection, cant display values yet

--- src/pages/forecasting/Forecasting.jsx
+++ src/pages/forecasting/Forecasting.jsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import './Forecasting.css'
 import { useEffect, useState } from "react"
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 import "../../components/chart/Chart.css";
 import {
@@ -13,7 +15,7 @@ import {
   Tooltip,
   ResponsiveContainer
 } from "recharts";
-import {ma} from 'moving-averages';
+import {ma,ema} from 'moving-averages';
 
 const useFetch = () => {
   const [dataAPI, setData] = useState(null);
@@ -29,107 +31,138 @@ const useFetch = () => {
     fetchData();
   }, []);
   return {dataAPI,loading};
-}
+};
 
 export default function Forecasting(){
-    const {dataAPI,loading} = useFetch()
-    if(dataAPI !== null){
-      var keys = Object.keys(dataAPI)
-      var values = Object.values(dataAPI)
-      const maValue = ma(values, 2)
-      console.log(maValue)
-      var data = [
-        {
-          name: keys[0],
-          actual: values[0],
-          forecast: maValue[0]
-        },
-        {
-          name: keys[1],
-          actual: values[1],
-          forecast: maValue[0]
-        },
-        {
-          name: keys[2],
-          actual: values[2],
-          forecast: maValue[1]
-        },
-        {
-          name: keys[3],
-          actual: values[3],
-          forecast: maValue[2]
-        },
-        {
-          name: keys[4],
-          actual: values[4],
-          forecast: maValue[3]
-        },
-        {
-          name: keys[5],
-          actual: values[5],
-          forecast: maValue[4]
-        },
-        {
-          name: keys[6],
-          actual: values[6],
-          forecast: maValue[5]
-        },
-        {
-          name: keys[7],
-          actual: values[7],
-          forecast: maValue[6]
-        },
-        {
-          name: keys[8],
-          actual: values[8],
-          forecast: maValue[7]
-        },
-        {
-          name: keys[9],
-          actual: values[9],
-          forecast: maValue[8]
-        },
-        {
-          name: keys[10],
-          actual: values[10],
-          forecast: maValue[9]
-        },
-        {
-          name: keys[11],
-          actual: values[11],
-          forecast: maValue[10]
-        },
-        {
-          name: keys[12],
-          actual: values[12],
-          forecast: maValue[11]
-        },
-      ]
-    };
-    return(
-        <div className="forecasting">
-          <div className='slider2'>
-            Moving Average, m = 2
-          </div>
-          <ResponsiveContainer width="97%" height={500} position="absolute">
-              <LineChart
-              data={data}
-              margin={{
-                  top: 30,
-                  right: 30,
-                  left: 30,
-                  bottom: 30
+  const [buttonValue, setValue] = useState('Moving Average, ⍺ = 2');
+  const handleChangeA = event => {
+    setValue('Moving Average, ⍺ = 2');
+  }
+  const handleChangeB = event => {
+    setValue('Expotential Smoothing, β = 0.2');
+  }
+  const handleChangeC = event => {
+    setValue('ARIMA');
+  }
+  const {dataAPI,loading} = useFetch()
+  if(dataAPI !== null){
+    var keys = Object.keys(dataAPI)
+    var values = Object.values(dataAPI)
+    const maValue = ma(values, 2)
+    console.log(maValue)
+    var data = [
+      {
+        name: keys[0],
+        actual: values[0],
+        forecast: maValue[0]
+      },
+      {
+        name: keys[1],
+        actual: values[1],
+        forecast: maValue[0]
+      },
+      {
+        name: keys[2],
+        actual: values[2],
+        forecast: maValue[1]
+      },
+      {
+        name: keys[3],
+        actual: values[3],
+        forecast: maValue[2]
+      },
+      {
+        name: keys[4],
+        actual: values[4],
+        forecast: maValue[3]
+      },
+      {
+        name: keys[5],
+        actual: values[5],
+        forecast: maValue[4]
+      },
+      {
+        name: keys[6],
+        actual: values[6],
+        forecast: maValue[5]
+      },
+      {
+        name: keys[7],
+        actual: values[7],
+        forecast: maValue[6]
+      },
+      {
+        name: keys[8],
+        actual: values[8],
+        forecast: maValue[7]
+      },
+      {
+        name: keys[9],
+        actual: values[9],
+        forecast: maValue[8]
+      },
+      {
+        name: keys[10],
+        actual: values[10],
+        forecast: maValue[9]
+      },
+      {
+        name: keys[11],
+        actual: values[11],
+        forecast: maValue[10]
+      },
+      {
+        name: keys[12],
+        actual: values[12],
+        forecast: maValue[11]
+      },
+    ]
+  };
+  return(
+    <div className="forecasting">
+      <div className='slider2'>
+        <div>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+            <Button 
+              onClick={() => {
+                handleChangeA()
               }}
-              >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis label={{ value: 'Quantity', angle: -90, position: 'insideLeft' }} />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="actual" stroke="#49454D" />
-              <Line type="monotone" dataKey="forecast" stroke="#AD6ADF" strokeWidth={3}/>
-              </LineChart>
-          </ResponsiveContainer>
+            >Moving Average</Button>
+            <Button
+              onClick={() => {
+                data = handleChangeB()
+              }}
+            >Expotential Smoothing</Button>
+            <Button
+              onClick={() => {
+                handleChangeC()
+              }}
+            >ARIMA</Button>
+          </ButtonGroup>
         </div>
-    )
+      </div>
+      <div className='slider2'>
+        {buttonValue}
+      </div>
+      <ResponsiveContainer width="97%" height={500} position="absolute">
+          <LineChart
+          data={data}
+          margin={{
+              top: 30,
+              right: 30,
+              left: 30,
+              bottom: 30
+          }}
+          >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis label={{ value: 'Quantity', angle: -90, position: 'insideLeft' }} />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="actual" stroke="#49454D" />
+          <Line type="monotone" dataKey="forecast" stroke="#AD6ADF" strokeWidth={3}/>
+          </LineChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
