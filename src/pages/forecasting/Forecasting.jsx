ded5a860efae5ea345bc008e469c57762d92168f
darentwh <87600423+darentwh@users.noreply.github.com>
2022-09-27 18:51:57 +0800

    3 options, able to display 3 different forecasts

--- src/pages/forecasting/Forecasting.jsx
+++ src/pages/forecasting/Forecasting.jsx
@@ -15,22 +15,20 @@ import {
   Tooltip,
   ResponsiveContainer
 } from "recharts";
-import {ma,ema} from 'moving-averages';
+import {ma,ema,wma} from 'moving-averages';
 
 const useFetch = () => {
   const [dataAPI, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
   useEffect((url = "https://7l5jx3guqnqr6gxnabjeb5icgm0evafu.lambda-url.us-east-1.on.aws/") => {
     async function fetchData(){
       const response = await fetch(url,{method:'GET'});
       const dataAPI = await response.json();
       console.log(dataAPI)
       setData(dataAPI);
-      setLoading(false);
     }
     fetchData();
   }, []);
-  return {dataAPI,loading};
+  return {dataAPI};
 };
 
 export default function Forecasting(){
@@ -39,105 +37,256 @@ export default function Forecasting(){
     setValue('Moving Average, ⍺ = 2');
   }
   const handleChangeB = event => {
-    setValue('Expotential Smoothing, β = 0.2');
+    setValue('Expotential Moving Average');
   }
   const handleChangeC = event => {
-    setValue('ARIMA');
+    setValue('Weighted Moving Average');
   }
-  const {dataAPI,loading} = useFetch()
+  const {dataAPI} = useFetch()
   if(dataAPI !== null){
     var keys = Object.keys(dataAPI)
     var values = Object.values(dataAPI)
-    const maValue = ma(values, 2)
-    console.log(maValue)
-    var data = [
-      {
-        name: keys[0],
-        actual: values[0],
-        forecast: maValue[0]
-      },
-      {
-        name: keys[1],
-        actual: values[1],
-        forecast: maValue[0]
-      },
-      {
-        name: keys[2],
-        actual: values[2],
-        forecast: maValue[1]
-      },
-      {
-        name: keys[3],
-        actual: values[3],
-        forecast: maValue[2]
-      },
-      {
-        name: keys[4],
-        actual: values[4],
-        forecast: maValue[3]
-      },
-      {
-        name: keys[5],
-        actual: values[5],
-        forecast: maValue[4]
-      },
-      {
-        name: keys[6],
-        actual: values[6],
-        forecast: maValue[5]
-      },
-      {
-        name: keys[7],
-        actual: values[7],
-        forecast: maValue[6]
-      },
-      {
-        name: keys[8],
-        actual: values[8],
-        forecast: maValue[7]
-      },
-      {
-        name: keys[9],
-        actual: values[9],
-        forecast: maValue[8]
-      },
-      {
-        name: keys[10],
-        actual: values[10],
-        forecast: maValue[9]
-      },
-      {
-        name: keys[11],
-        actual: values[11],
-        forecast: maValue[10]
-      },
-      {
-        name: keys[12],
-        actual: values[12],
-        forecast: maValue[11]
-      },
-    ]
+    if(buttonValue === 'Moving Average, ⍺ = 2'){
+      const maValue = ma(values, 2)
+      console.log(maValue)
+      var data = [
+        {
+          name: keys[0],
+          actual: values[0],
+          forecast: maValue[0]
+        },
+        {
+          name: keys[1],
+          actual: values[1],
+          forecast: maValue[0]
+        },
+        {
+          name: keys[2],
+          actual: values[2],
+          forecast: maValue[1]
+        },
+        {
+          name: keys[3],
+          actual: values[3],
+          forecast: maValue[2]
+        },
+        {
+          name: keys[4],
+          actual: values[4],
+          forecast: maValue[3]
+        },
+        {
+          name: keys[5],
+          actual: values[5],
+          forecast: maValue[4]
+        },
+        {
+          name: keys[6],
+          actual: values[6],
+          forecast: maValue[5]
+        },
+        {
+          name: keys[7],
+          actual: values[7],
+          forecast: maValue[6]
+        },
+        {
+          name: keys[8],
+          actual: values[8],
+          forecast: maValue[7]
+        },
+        {
+          name: keys[9],
+          actual: values[9],
+          forecast: maValue[8]
+        },
+        {
+          name: keys[10],
+          actual: values[10],
+          forecast: maValue[9]
+        },
+        {
+          name: keys[11],
+          actual: values[11],
+          forecast: maValue[10]
+        },
+        {
+          name: keys[12],
+          actual: values[12],
+          forecast: maValue[11]
+        },
+      ];
+    } if(buttonValue === 'Expotential Moving Average'){
+      const maValue = ema(values, 2)
+      console.log(maValue)
+      data = [
+        {
+          name: keys[0],
+          actual: values[0],
+          forecast: maValue[0]
+        },
+        {
+          name: keys[1],
+          actual: values[1],
+          forecast: maValue[0]
+        },
+        {
+          name: keys[2],
+          actual: values[2],
+          forecast: maValue[1]
+        },
+        {
+          name: keys[3],
+          actual: values[3],
+          forecast: maValue[2]
+        },
+        {
+          name: keys[4],
+          actual: values[4],
+          forecast: maValue[3]
+        },
+        {
+          name: keys[5],
+          actual: values[5],
+          forecast: maValue[4]
+        },
+        {
+          name: keys[6],
+          actual: values[6],
+          forecast: maValue[5]
+        },
+        {
+          name: keys[7],
+          actual: values[7],
+          forecast: maValue[6]
+        },
+        {
+          name: keys[8],
+          actual: values[8],
+          forecast: maValue[7]
+        },
+        {
+          name: keys[9],
+          actual: values[9],
+          forecast: maValue[8]
+        },
+        {
+          name: keys[10],
+          actual: values[10],
+          forecast: maValue[9]
+        },
+        {
+          name: keys[11],
+          actual: values[11],
+          forecast: maValue[10]
+        },
+        {
+          name: keys[12],
+          actual: values[12],
+          forecast: maValue[11]
+        },
+      ]
+    } if(buttonValue === 'Weighted Moving Average'){
+      const maValue = wma(values, 2)
+      console.log(maValue)
+      data = [
+        {
+          name: keys[0],
+          actual: values[0],
+          forecast: maValue[0]
+        },
+        {
+          name: keys[1],
+          actual: values[1],
+          forecast: maValue[0]
+        },
+        {
+          name: keys[2],
+          actual: values[2],
+          forecast: maValue[1]
+        },
+        {
+          name: keys[3],
+          actual: values[3],
+          forecast: maValue[2]
+        },
+        {
+          name: keys[4],
+          actual: values[4],
+          forecast: maValue[3]
+        },
+        {
+          name: keys[5],
+          actual: values[5],
+          forecast: maValue[4]
+        },
+        {
+          name: keys[6],
+          actual: values[6],
+          forecast: maValue[5]
+        },
+        {
+          name: keys[7],
+          actual: values[7],
+          forecast: maValue[6]
+        },
+        {
+          name: keys[8],
+          actual: values[8],
+          forecast: maValue[7]
+        },
+        {
+          name: keys[9],
+          actual: values[9],
+          forecast: maValue[8]
+        },
+        {
+          name: keys[10],
+          actual: values[10],
+          forecast: maValue[9]
+        },
+        {
+          name: keys[11],
+          actual: values[11],
+          forecast: maValue[10]
+        },
+        {
+          name: keys[12],
+          actual: values[12],
+          forecast: maValue[11]
+        },
+      ]
+    };
   };
   return(
     <div className="forecasting">
       <div className='slider2'>
         <div>
-          <ButtonGroup variant="contained" aria-label="outlined primary button group">
-            <Button 
+          <ButtonGroup color="secondary" variant="contained" aria-label="outlined primary button group">
+            <Button
+              style={{
+                backgroundColor: "#AD6ADF",
+              }}   
               onClick={() => {
                 handleChangeA()
               }}
             >Moving Average</Button>
             <Button
+              style={{
+                backgroundColor: "#AD6ADF",
+              }}
               onClick={() => {
-                data = handleChangeB()
+                handleChangeB()
               }}
             >Expotential Smoothing</Button>
             <Button
+              style={{
+                backgroundColor: "#AD6ADF",
+              }}
               onClick={() => {
                 handleChangeC()
               }}
-            >ARIMA</Button>
+            >Weighted Moving Average</Button>
           </ButtonGroup>
         </div>
       </div>
@@ -145,15 +294,15 @@ export default function Forecasting(){
         {buttonValue}
       </div>
       <ResponsiveContainer width="97%" height={500} position="absolute">
-          <LineChart
-          data={data}
-          margin={{
-              top: 30,
-              right: 30,
-              left: 30,
-              bottom: 30
-          }}
-          >
+        <LineChart
+        data={data}
+        margin={{
+            top: 30,
+            right: 30,
+            left: 30,
+            bottom: 30
+        }}
+        >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis label={{ value: 'Quantity', angle: -90, position: 'insideLeft' }} />
@@ -161,7 +310,7 @@ export default function Forecasting(){
           <Legend />
           <Line type="monotone" dataKey="actual" stroke="#49454D" />
           <Line type="monotone" dataKey="forecast" stroke="#AD6ADF" strokeWidth={3}/>
-          </LineChart>
+        </LineChart>
       </ResponsiveContainer>
     </div>
   )
