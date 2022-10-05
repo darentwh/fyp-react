80e8df47d4111f83f5c08a81c1b687f3b8dd1bd5
darentwh <87600423+darentwh@users.noreply.github.com>
2022-10-05 20:33:02 +0800

    swap mps and mrp order. removed random no api call

--- src/pages/masterproductionscheduling/Masterproductionscheduling.jsx
+++ src/pages/masterproductionscheduling/Masterproductionscheduling.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import { useEffect, useState } from "react";
 import './Masterproductionscheduling.css'
-import Chart from "../../components/chart/Chart"
 
-export default function Masterproductionscheduling(){  
-  const useFetch = () => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect((url = "https://xxdzzkf7xczg65izs37npoc3c40biikv.lambda-url.us-east-1.on.aws/") => {
-      async function fetchData(){
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data)
-        setData(data);
-        setLoading(false);
-      }
-      fetchData();
-    }, []);
-    return {data,loading};
-  }
-  const {data, loading} = useFetch()
+export default function Masterproductionscheduling(){
   return(
     <div className="mps">
       API Call Values:
-      <br/>
-      <div className="chartDiv">
-        {loading ? <div>Loading...</div> : <Chart value={data}/>}
-      </div>
     </div>
   )
 }
