62560552143c7c2f4c078f597f137a5457bf5ab3
darentwh <darentan1998@gmail.com>
2022-10-26 23:03:19 +0800

    minor edits to home page description

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,8 @@ export default function Home(){
                     <b>Proj No</b>: C105 <br/>
                     <b>Main Supervisor</b>: Assoc Prof Rajesh Piplani <br/>
                     <b>Title</b>: Development of an application for production scheduling and requirement planning	<br/>
-                    <b>Description</b>: Scope would be limited to planning for a mid-size company, using same data, but allowing data to be ported from one application to another, including any changes to data. Data will be developed based on reference material.
+                    <b>Objective</b>: Develop an computer-based tool that allows <b>forecasting, master production scheduling, and material requirement planning</b> for data seamlessly and in an integrated manner. The tool should be able to develop plans for different set of customer orders/forecast input. <br/>
+                    <b>Scope</b>: Scope would be limited to planning for a mid-size company, using same data, but allowing data to be ported from one application to another, including any changes to data. Data will be developed based on reference material.
                 </div>
             </div>
         </div>
