2f847cad63668abdf7705a4f6ce537832aefda0a
darentwh <darentan1998@gmail.com>
2022-12-12 17:00:27 +0800

    BOM and Product Structure

--- src/pages/mrp/mrp.jsx
+++ src/pages/mrp/mrp.jsx
@@ -9,7 +9,11 @@ import Paper from '@mui/material/Paper';
 import Slider from '@mui/material/Slider'
 import { useEffect, useState } from "react"
 import {ma} from 'moving-averages';
+//import Tree, { withStyles } from 'react-vertical-tree'
 import './mrp.css'
+
+import { Tree, TreeNode } from 'react-organizational-chart';
+import styled from 'styled-components';
 
 const useFetch = () => {
     const [dataAPI, setData] = useState(null);
@@ -29,11 +33,22 @@ function createData(name, calories) {
     return { name, calories};
 }
 
+const styles = {
+    lines: {
+      color: '#AD6ADF',
+      height: '100px',
+    },
+    node: {
+      backgroundColor: '#fff',
+      border: '1px solid #000000',
+    },
+    text: {
+      color: '#000000',
+    }
+}
+//const StyledTree = withStyles(styles)(Tree)
+
 export default function Mrp(){  
-    const [message, setMessage] = React.useState('0');
-    const handleChangeFour = event => {
-        setMessage(event.target.value);
-    };
     const {dataAPI} = useFetch()
     if(dataAPI !== null){
         var keys = Object.keys(dataAPI)
@@ -43,18 +58,30 @@ export default function Mrp(){
         console.log(maValue)
         var nextMonthForecast = maValue[11]
         var roundedForecast = parseInt(Math.ceil(nextMonthForecast / 10) * 10);
-        var noOfParts = (roundedForecast*5)
+        var noOfParts = (roundedForecast)
         console.log(noOfParts)
     };
     var rows = [
-        createData('721A-12', noOfParts),
-        createData('138N', noOfParts),
-        createData('721-33', noOfParts),
-        createData('721-300', noOfParts),
-        createData('721-310', noOfParts),
-        createData('RK1-720', noOfParts),
-        createData('IS720', noOfParts),
-        createData('9B5X7', noOfParts),
+        createData('Canopy Screw, 721-11', 3*noOfParts),
+        createData('Canopy, 721-3', noOfParts),
+        createData('Bonnet, 721-20', noOfParts),
+        createData('Plastic Washer, 721A-12', noOfParts),
+        createData('O-Ring, WK-138N', noOfParts),
+        createData('Load Nut & Guide, 721-90', noOfParts),
+        createData('Load Washer, 721-80', noOfParts),
+        createData('Disc Upper, 721A-34', noOfParts),
+        createData('Poppet, 721-30', noOfParts),
+        createData('Spring, 721-33', noOfParts),
+        createData('Screw, 721-11A', noOfParts),
+        createData('Lower Disc, 721B-34', noOfParts),
+        createData('Guide Spider, 721-31', noOfParts),
+        createData('Hex Nut, 721-9A', noOfParts),
+        createData('Test Cock, 18-860XL', 2*noOfParts),
+        createData('Ball Valve 1/2" Tap, 12-850T', 2*noOfParts),
+        createData('Ball Valve 3/4" Tap, 34-850T', 2*noOfParts),
+        createData('Ball Valve 1" Tap, 1-850T', 2*noOfParts),
+        createData('Poppet Assembly, 721-300', noOfParts),
+        createData('Spider Assembly, 721-310', noOfParts),
     ];
     const [value, setValue] = React.useState(10);
     const handleChange = (event, newValue) => {
@@ -62,118 +89,135 @@ export default function Mrp(){
           setValue(newValue);
         }
     };
-    const [valuetwo, setValuetwo] = React.useState(10);
-    const handleChangeTwo = (event, newValue) => {
-        if (typeof newValue === 'number') {
-          setValuetwo(newValue);
-        }
-    };
-    const [valuethree, setValuethree] = React.useState(10);
-    const handleChangeThree = (event, newValue) => {
-        if (typeof newValue === 'number') {
-          setValuethree(newValue);
-        }
-    };
-    const multiply = (num1,num2,num3)=>{
-        return num1 * num2 * num3
-    };
-    var intOne = parseInt(value)
-    var intTwo = parseInt(valuetwo)
-    var intThree = parseInt(valuethree)
-    let multiplyResult = multiply(intOne,intTwo,intThree)
-    ;
+    const  data = [
+        {id: 1, name: 'Pressure Vacuum Breaker : '+noOfParts, parent: null, children: [
+            {id: 2, parent: {id: 1}, name: 'Canopy Screw : '+3*noOfParts, children: []},
+            {id: 3, parent: {id: 1}, name: 'Canopy : '+noOfParts, children: []},
+            {id: 4, parent: {id: 1}, name: 'Bonnet : '+noOfParts, children: []},
+            {id: 5, parent: {id: 1}, name: 'Plastic Washer : '+noOfParts, children: []},
+            {id: 6, parent: {id: 1}, name: 'O-Ring : '+noOfParts, children: []},
+            {id: 8, parent: {id: 1}, name: 'Poppet Assembly', children: [
+                {id: 9, parent: {id: 8}, name: 'Load Nut & Guide : '+noOfParts, children: []},
+                {id: 10, parent: {id: 8}, name: 'Load Washer : '+noOfParts, children: []},
+                {id: 11, parent: {id: 8}, name: 'Disc Upper : '+noOfParts, children: []},
+                {id: 12, parent: {id: 8}, name: 'Poppet : '+noOfParts, children: []},
+            ]},
+            {id: 13, parent: {id: 1}, name: 'Spring : '+noOfParts, children: []},
+            {id: 14, parent: {id: 1}, name: 'Spider Assembly', children: [
+                {id: 15, parent: {id: 14}, name: 'Screw : '+noOfParts, children: []},
+                {id: 16, parent: {id: 14}, name: 'Lower Disc : '+noOfParts, children: []},
+                {id: 17, parent: {id: 14}, name: 'Guide Spider : '+noOfParts, children: []},
+                {id: 18, parent: {id: 14}, name: 'Hex Nut : '+noOfParts, children: []},
+            ]},
+            {id: 19, parent: {id: 1}, name: 'Test Cock : '+2*noOfParts, children: []},
+            {id: 20, parent: {id: 1}, name: 'Ball Valve 1/2" Tap : '+2*noOfParts, children: []},
+            {id: 21, parent: {id: 1}, name: 'Ball Valve 3/4" Tap : '+2*noOfParts, children: []},
+            {id: 22, parent: {id: 1}, name: 'Ball Valve 1" Tap : '+2*noOfParts, children: []},
+        ]}
+        ]      
+
+        const StyledNode = styled.div`
+            padding: 1px;
+            border-radius: 8px;
+            display: inline-block;
+            border: 1px solid red;
+        `;
+
     return(
         <div className="mrp">
-            <div className="featured">
-                <div className="featuredItem">
-                    <span className="featuredTitle">No. of Units</span>
-                    <div className="slider2">    
-                        <input
-                            type="text"
-                            id="message"
-                            onChange={handleChangeFour}
-                            name="message"
-                            value={message}
-                            autoComplete="off"
-                        />
-                    </div>
+            <div className='featureItem'>
+                <div align='center'>
                     Month: <b>{nextMonth}</b><br/>
                     Forecasted Demand: <b>{nextMonthForecast}</b><br/>
                     Rounded Demand: <b>{roundedForecast}</b>
                 </div>
+            </div>
+            <div className="featured">
                 <div className="featuredItem">
-                    <span className="featuredTitle">Forecasted Required Component Quantity</span>
+                    <span className="featuredTitle">Product Structure Diagram</span>
+                    <div>
+                        {/*<StyledTree data={data} direction/>*/}
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <div className="featuredItem">
+                    <span className="featuredTitle">Bill of Material</span>
                     <div className="slider2">
-                        <TableContainer component={Paper} align='center'>
-                            <Table sx={{ minWidth: "100%" , maxWidth:"100%"}} aria-label="simple table" style={{ width: '80%' }}>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="left">Part No.</TableCell>
-                                        <TableCell>Quantity , <i>5/unit</i></TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                {rows.map((row) => (
-                                    <TableRow
-                                    key={row.name}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                    <TableCell component="th" scope="row" style={{ width: '5%' }}>
-                                        {row.name}
-                                    </TableCell>
-                                    <TableCell style={{ width: '20%' }}>{row.calories}</TableCell>
-                                    </TableRow>
-                                ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+                        <div className='tableContainer'>
+                            <TableContainer component={Paper} align='center'>
+                                <Table sx={{ minWidth: "100%" , maxWidth:"100%"}} aria-label="simple table" style={{ width: '80%' }}>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="left"><b>Part No.</b></TableCell>
+                                            <TableCell><b>Quantity Required</b></TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                    {rows.map((row) => (
+                                        <TableRow
+                                        key={row.name}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                        <TableCell component="th" scope="row" style={{ width: '10%' }}>
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell style={{ width: '5%' }}>{row.calories}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className = "featured">
                 <div className="featuredItem">
-                    <span className="featuredTitle">Material 1</span>
+                    <span className="featuredTitle">Forecast Period</span>
                     <Slider
                         value={value}
                         size="small"
-                        defaultValue={70}
+                        min={1}
+                        max={10}
+                        defaultValue={1}
                         aria-label="Small"
                         valueLabelDisplay="auto"
                         onChange={handleChange}
                         color="secondary"
                     />
-                    {value}
-                </div>
-                <div className="featuredItem">
-                    <span className="featuredTitle">Material 2</span>
-                    <Slider
-                        value={valuetwo}
-                        size="small"
-                        defaultValue={70}
-                        aria-label="Small"
-                        valueLabelDisplay="auto"
-                        onChange={handleChangeTwo}
-                        color="secondary"
-                    />
-                    {valuetwo}
-                </div>
-                <div className="featuredItem">
-                    <span className="featuredTitle">Material 3</span>
-                    <Slider
-                        value={valuethree}
-                        size="small"
-                        defaultValue={70}
-                        aria-label="Small"
-                        valueLabelDisplay="auto"
-                        onChange={handleChangeThree}
-                        color="secondary"
-                    />
-                    {valuethree}
+                    <span className='slider2'>{value}</span>
                 </div>
             </div>
-            <div className="slider2">
-                Total Multiplied: {multiplyResult}
-            </div>
+            <Tree
+                lineWidth={'2px'}
+                lineColor={'green'}
+                lineBorderRadius={'10px'}
+                label={<StyledNode>Pressure Vacuum Breaker</StyledNode>}
+            >
+                <TreeNode label={<StyledNode>Canopy Screw{'\n'+noOfParts}</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Canopy</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Bonnet</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Plastic Washer</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>O-Ring</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Poppet Assembly</StyledNode>}>
+                <TreeNode label={<StyledNode>Load Nut & Guide</StyledNode>} />
+                <TreeNode label={<StyledNode>Load Washer</StyledNode>} />
+                <TreeNode label={<StyledNode>Disc Upper</StyledNode>} />
+                <TreeNode label={<StyledNode>Poppet</StyledNode>} />
+                </TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                <TreeNode label={<StyledNode>Child 1</StyledNode>}></TreeNode>
+                
+            </Tree>
         </div>
     )
 }
--- src/pages/ordering/Ordering.jsx
+++ src/pages/ordering/Ordering.jsx
@@ -9,7 +9,7 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
 import Autocomplete from '@mui/material/Autocomplete';
 
 const optionsCountries = ['Singapore', 'Malaysia'];
-const optionsItem = ['RK1-720'];
+const optionsItem = ['34-720A'];
 
 export default function Ordering(){
     function callApi() {
@@ -44,7 +44,7 @@ export default function Ordering(){
     
     return(
         <div className='ordering'>
-            <div className='orderTitle'>Order Form ✍🏻</div>
+            <div className='orderTitle'>Order Form</div>
             <div className='featuredItem'>
                 <div className='featured2'>
                     <div>
